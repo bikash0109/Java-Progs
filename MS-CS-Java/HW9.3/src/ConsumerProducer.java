50bee33b10596ab1a464e9b681e22ba777574abe
Bikash Roy <bikash0109@gmail.com>
2018-10-24 21:54:13 -0400

    ok

--- MS-CS-Java/HW9.3/src/ConsumerProducer.java
+++ MS-CS-Java/HW9.3/src/ConsumerProducer.java
@@ -1,53 +1,39 @@
 
 public class ConsumerProducer {
-
     static MyStorage<Integer> list = new MyStorage<>();
     int producerItem, consumerItem, capacity;
-
     ConsumerProducer(int producerItem, int consumerItem, int capacity){
         this.capacity = capacity;
         this.producerItem = producerItem;
         this.consumerItem = consumerItem;
     }
-
     // Function called by producer thread
     public void produce() throws InterruptedException {
         int value = 0;
         while (true) {
             synchronized (this) {
-
-                while (list.size() == capacity) {
+                while (list.size() == producerItem) {
                     wait();
                 }
-
                 System.out.println(Thread.currentThread().getName().split("-")[1] + " produce : " + value);
-
                 list.add(value++);
-
                 // wake up consumer thread
                 notifyAll();
-
                 Thread.sleep(1000);
             }
         }
     }
-
     // Function called by consumer thread
     public void consume() throws InterruptedException {
         while (true) {
             synchronized (this) {
-
                 while (list.size() == 0) {
                     wait();
                 }
-
                 int val = list.removeFirst();
-
                 System.out.println(Thread.currentThread().getName().split("-")[1] + " consumer : " + val);
-
                 // Wake up producer thread
                 notifyAll();
-
                 Thread.sleep(1000);
             }
         }
@@ -55,12 +41,11 @@ public class ConsumerProducer {
 
     public static void main(String[] args) {
 
-        int numOfProducerThread = Integer.parseInt(args[0]);
-        int producerItems = Integer.parseInt(args[1]);
-        int numOfConsumerThread = Integer.parseInt(args[2]);
-        int consumerItems = Integer.parseInt(args[3]);
+        int numOfProducerThread = Integer.parseInt(args[2]);
+        int producerItems = Integer.parseInt(args[3]);
+        int numOfConsumerThread = Integer.parseInt(args[0]);
+        int consumerItems = Integer.parseInt(args[2]);
         int capacity = Integer.parseInt(args[4]);
-
 
         final ConsumerProducer consumerProducer = new ConsumerProducer(producerItems, consumerItems, capacity);
 
