0ec81e553738fa26d8d24b0b45b3cf809afa920c
Bikash Roy <bikash0109@gmail.com>
2018-10-25 07:41:05 -0400

    lol

--- MS-CS-Java/ThreadsTest/src/Thread_5.java
+++ MS-CS-Java/ThreadsTest/src/Thread_5.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Thread_5 extends Thread    {
     private String info;
-    Vector aVector;
+    static Vector aVector;
 
     public Thread_5 (String info, Vector aVector) {
         this.info = info;
@@ -14,11 +14,13 @@ public class Thread_5 extends Thread    {
             System.err.println(info + ": is in protected()");
             try {
                 if ( info.equals("second") )
-                    sleep(1000);
+                    System.out.println("sec");
+                    //sleep(1000);
                 else
-                    sleep(3000);
+                    System.out.println("1st");
+                    //sleep(3000);
             }
-            catch (  InterruptedException e ) {
+            catch (  Exception e ) {
                 System.err.println("Interrupted!");
             }
             System.err.println(info + ": exit run");
@@ -34,7 +36,7 @@ public class Thread_5 extends Thread    {
         Thread_5 aT5_0 = new Thread_5("first", aVector);
         Thread_5 aT5_1 = new Thread_5("second", aVector);
 
-        aT5_0.start();
         aT5_1.start();
+        aT5_0.start();
     }
 }
